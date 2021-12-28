1f015629be100bfd0f178f63acb361215f0aabff
adrytech <89694855+adrytech@users.noreply.github.com>
2021-12-28 18:53:20 +0100

    Add files via upload

---
+++ Java Lessons/WhileDo/src/WhileDo.java
@@ -0,0 +1,25 @@
+public class WhileDo {
+
+    public static void main(String[] args) {
+/*
+
+While -->
+
+* */
+        int number = 0;
+
+        while (number < 7){
+            System.out.println(number);
+            number++;
+        }
+/*
+
+Do While -->
+
+* */
+        do{
+            System.out.println(number);
+            number++;
+        }while(number < 7);
+    }
+}
